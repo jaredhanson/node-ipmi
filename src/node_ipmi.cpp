1ee6254706061caac78761e47d7ebf0707c64b30
Eric Litak <elitak@gmail.com>
2013-06-27 16:57:42 -0700

    sorta runs again

--- src/node_ipmi.cpp
+++ src/node_ipmi.cpp
@@ -27,52 +27,43 @@ NodeIpmi::~NodeIpmi() {
 	interface->close(interface);
 }
 
-/*
 //V8_ESCTOR(NodeIpmi) { V8_CTOR_NO_JS }
 
-NODE_ETYPE(NodeIpmi, "NodeIpmi") {
-	//Local<Function> func = templ->GetFunction();
-	target->Set(v8u::Symbol("init"), v8u::Func(Init)->GetFunction());
 
-	//NODE_SET_PROTOTYPE_METHOD(templ, "getChassisPowerStatus", GetChassisPowerStatus);
-} NODE_TYPE_END()
-V8_POST_TYPE(NodeIpmi);
-
-V8_SCB(NodeIpmi::Init) {
+V8_SCB(NodeIpmi::LoadInterface) {
 	if(args.Length() != 1) V8_STHROW(v8u::RangeErr("Invalid Argument"));
 
-        String::Utf8Value int_name(args[0]->ToString());
+        String::Utf8Value interface_name(args[0]->ToString());
 
-	ipmi_intf *intf = ipmi_intf_load(*int_name);
+	ipmi_intf *intf = ipmi_intf_load(*interface_name);
         //int argc = 11; char *argv[] = { "ipmitool", "-I", "lan", "-H", "192.168.0.30", "-U", "root", "-P", "calvin", "power", "status" };
         //int argc = 2; char *argv[] = {"ipmitool", "-h"};
         //int rc = ipmi_main(argc, argv, ipmitool_cmd_list, NULL);
 
         if (intf == NULL) {
-           std::sstringstream msg;
-           V8_STHROW(v8u::Err((msg << "Error loading interface named \"" << int_name << "\"")));
+         std::stringstream msg;
+         msg << "Error loading interface named \"" << *interface_name << "\"";
+         V8_STHROW(v8u::Err(msg.str().c_str()));
         }
 	//return (new NodeIpmi(intf))->Wrapped();
 }
-*/
 
-/*
 V8_CB(NodeIpmi::GetChassisPowerStatus) {
+        String::Utf8Value interface_name(args[0]->ToString());
+
+	ipmi_intf *interface = ipmi_intf_load(*interface_name);
   ipmi_intf_session_set_hostname(interface, "192.168.0.30");
   ipmi_intf_session_set_username(interface, "root");
   ipmi_intf_session_set_password(interface, "calvin");
   int chassis_status = ipmi_chassis_power_status(interface);
   if (chassis_status == -1) {
-     V8_STHROW(v8u::Err("call failed"));
-     std::sstringstream msg;
-     V8_STHROW(v8u::Err((
-           (msg << "a" << "." << "B").str()
-     )); //FIXME concat without std or malloc?
+     std::stringstream msg;
+     msg << "call failed: " << "GetChassisPowerStatus";
+     V8_STHROW(v8u::Err(msg.str().c_str()));
   }
-  V8_RET(chassis_status);
+  return  Number::New(chassis_status);
 } V8_CB_END()
-*/
 
 NODE_DEF_MAIN() {
-//	NodeIpmi::init(target);
+	NodeIpmi::init(target);
 } NODE_DEF_MAIN_END(ipmi)
