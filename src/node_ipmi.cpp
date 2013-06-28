24c73b57dafbb7bea5375bbbd132d238f580c117
Eric Litak <elitak@gmail.com>
2013-06-27 18:48:27 -0700

    spacing changes only

--- src/node_ipmi.cpp
+++ src/node_ipmi.cpp
@@ -14,72 +14,72 @@ int verbose = 0;
 int csv_output = 0;
 
 NodeIpmi::NodeIpmi(const char *interface_name) {
-   interface = ipmi_intf_load((char *)interface_name);
+    interface = ipmi_intf_load((char *)interface_name);
 
-   if (interface == NULL) {
-      std::stringstream msg;
-      msg << "Error loading interface named \"" << interface_name << "\"";
-      //FIXME NO RETURNS ALLOWED MAKE STATIC CONST//V8_STHROW(v8u::Err(msg.str().c_str()));
-   }
+    if (interface == NULL) {
+        std::stringstream msg;
+        msg << "Error loading interface named \"" << interface_name << "\"";
+        //FIXME NO RETURNS ALLOWED MAKE STATIC CONST//V8_STHROW(v8u::Err(msg.str().c_str()));
+    }
 }
 
 NodeIpmi::~NodeIpmi() {
-	interface->close(interface);
+    interface->close(interface);
 }
 
 //V8_ESCTOR(NodeIpmi) { V8_CTOR_NO_JS }
 
 
 V8_SCB(NodeIpmi::LoadInterface) {
-	if(args.Length() != 1) V8_STHROW(v8u::RangeErr("Invalid Argument"));
+    if(args.Length() != 1) V8_STHROW(v8u::RangeErr("Invalid Argument"));
 
-        String::Utf8Value interface_name(args[0]->ToString());
+    String::Utf8Value interface_name(args[0]->ToString());
 
-	ipmi_intf *intf = ipmi_intf_load(*interface_name);
-        //int argc = 11; char *argv[] = { "ipmitool", "-I", "lan", "-H", "192.168.0.30", "-U", "root", "-P", "calvin", "power", "status" };
-        //int argc = 2; char *argv[] = {"ipmitool", "-h"};
-        //int rc = ipmi_main(argc, argv, ipmitool_cmd_list, NULL);
+    ipmi_intf *intf = ipmi_intf_load(*interface_name);
+    //int argc = 11; char *argv[] = { "ipmitool", "-I", "lan", "-H", "192.168.0.30", "-U", "root", "-P", "calvin", "power", "status" };
+    //int argc = 2; char *argv[] = {"ipmitool", "-h"};
+    //int rc = ipmi_main(argc, argv, ipmitool_cmd_list, NULL);
 
-        if (intf == NULL) {
-         std::stringstream msg;
-         msg << "Error loading interface named \"" << *interface_name << "\"";
-         V8_STHROW(v8u::Err(msg.str().c_str()));
-        }
-	//return (new NodeIpmi(intf))->Wrapped();
+    if (intf == NULL) {
+        std::stringstream msg;
+        msg << "Error loading interface named \"" << *interface_name << "\"";
+        V8_STHROW(v8u::Err(msg.str().c_str()));
+    }
+    //return (new NodeIpmi(intf))->Wrapped();
 }
 
-        V8_CB(NodeIpmi::GetChassisPowerStatus) {
-           v8::String::Utf8Value interface_name(args[0]->ToString());
-           NodeIpmi *self = Unwrap(args.This());
-           ipmi_intf_session_set_username(self->interface, "root");
-           ipmi_intf_session_set_password(self->interface, "calvin");
-           int chassis_status = ipmi_chassis_power_status(self->interface);
-           if (chassis_status == -1) {
-              std::stringstream msg;
-              msg << "call failed: " << "GetChassisPowerStatus";
-              V8_STHROW(v8u::Err(msg.str().c_str()));
-           }
-           //V8_RET(chassis_status);
-           return v8::Integer::New(chassis_status);
-        } V8_CB_END()
+V8_CB(NodeIpmi::GetChassisPowerStatus) {
+    v8::String::Utf8Value interface_name(args[0]->ToString());
+    NodeIpmi *self = Unwrap(args.This());
+    ipmi_intf_session_set_username(self->interface, "root");
+    ipmi_intf_session_set_password(self->interface, "calvin");
+    int chassis_status = ipmi_chassis_power_status(self->interface);
+    if (chassis_status == -1) {
+        std::stringstream msg;
+        msg << "call failed: " << "GetChassisPowerStatus";
+        V8_STHROW(v8u::Err(msg.str().c_str()));
+    }
+    //V8_RET(chassis_status);
+    return v8::Integer::New(chassis_status);
+} V8_CB_END()
 
-        V8_ESET(NodeIpmi, SetHostname) {
-           if (!value->IsString()) {
-              V8_THROW(v8u::Err("hostname must be a string"));
-           }
+V8_ESET(NodeIpmi, SetHostname) {
+    if (!value->IsString()) {
+        V8_THROW(v8u::Err("hostname must be a string"));
+    }
 
-          NodeIpmi* self = Unwrap(info.Holder());
-          self->hostname = value->ToString();
-          String::Utf8Value hostname(self->hostname);
-          ipmi_intf_session_set_hostname(self->interface, *hostname);
-        } V8_SET_END()
+    NodeIpmi* self = Unwrap(info.Holder());
+    self->hostname = value->ToString();
+    String::Utf8Value hostname(self->hostname);
+    ipmi_intf_session_set_hostname(self->interface, *hostname);
+} V8_SET_END()
 
 
-        V8_EGET(NodeIpmi, GetHostname) {
-          NodeIpmi* self = Unwrap(info.Holder());
-          V8_RET(self->hostname);
-        } V8_GET_END()
+V8_EGET(NodeIpmi, GetHostname) {
+    NodeIpmi* self = Unwrap(info.Holder());
+    V8_RET(self->hostname);
+} V8_GET_END()
 
 NODE_DEF_MAIN() {
-	NodeIpmi::init(target);
+    NodeIpmi::init(target);
 } NODE_DEF_MAIN_END(ipmi)
--- src/node_ipmi.hpp
+++ src/node_ipmi.hpp
@@ -6,32 +6,32 @@ extern "C" {
 using namespace v8;
 
 class NodeIpmi : public node::ObjectWrap {
-public:
-	NodeIpmi(const char *interface_name);
-	~NodeIpmi();
+   public:
+      NodeIpmi(const char *interface_name);
+      ~NodeIpmi();
 
-	V8_CTOR() {
-          if(args.Length() != 1) V8_STHROW(v8u::RangeErr("Invalid Argument"));
-          String::Utf8Value interface_name(args[0]->ToString());
-          V8_WRAP(new NodeIpmi(*interface_name));
-        } V8_CTOR_END()
+      V8_CTOR() {
+         if(args.Length() != 1) V8_STHROW(v8u::RangeErr("Invalid Argument"));
+         String::Utf8Value interface_name(args[0]->ToString());
+         V8_WRAP(new NodeIpmi(*interface_name));
+      } V8_CTOR_END()
 
-        void setHostname(Local<String> hostname) {this->hostname = hostname;}
+      void setHostname(Local<String> hostname) {this->hostname = hostname;}
 
-        static V8_SCB(GetChassisPowerStatus);
-	static V8_SCB(LoadInterface);
-        V8_SSET(SetHostname);
-        V8_SGET(GetHostname);
+      static V8_SCB(GetChassisPowerStatus);
+      static V8_SCB(LoadInterface);
+      V8_SSET(SetHostname);
+      V8_SGET(GetHostname);
 
-        NODE_TYPE(NodeIpmi, "NodeIpmi") {
-          V8_DEF_CB("getChassisPowerStatus", GetChassisPowerStatus);
-          V8_DEF_ACC("hostname", GetHostname, SetHostname);
-          //V8_DEF_ACC("username", GetUsername, SetUsername);
-          //V8_DEF_ACC("password", GetPassword, SetPassword);
-        } NODE_TYPE_END()
-private:
-	ipmi_intf *interface;
-        Local<String> hostname;
+      NODE_TYPE(NodeIpmi, "NodeIpmi") {
+         V8_DEF_CB("getChassisPowerStatus", GetChassisPowerStatus);
+         V8_DEF_ACC("hostname", GetHostname, SetHostname);
+         //V8_DEF_ACC("username", GetUsername, SetUsername);
+         //V8_DEF_ACC("password", GetPassword, SetPassword);
+      } NODE_TYPE_END()
+   private:
+      ipmi_intf *interface;
+      Local<String> hostname;
 };
 V8_POST_TYPE(NodeIpmi)
 
